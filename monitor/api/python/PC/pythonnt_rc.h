3e32c6168226907212ed5dd90e28bee2a8c0f228
FFMG <github@myoddweb.com>
2016-02-27 15:42:25 +0200

    Added python 3.5.1

---
+++ monitor/api/python/PC/pythonnt_rc.h
@@ -0,0 +1,4 @@
+/* This file created by python.props /t:GeneratePythonNtRcH */
+#define FIELD3 1150
+#define MS_DLL_ID "3.5"
+#define PYTHON_DLL_NAME "python6435.dll"
---
+++ monitor/api/python/PC/pythonnt_rc_d.h
@@ -0,0 +1,4 @@
+/* This file created by python.props /t:GeneratePythonNtRcH */
+#define FIELD3 1150
+#define MS_DLL_ID "3.5-32"
+#define PYTHON_DLL_NAME "python35_d.dll"
