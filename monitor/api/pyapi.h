33241fd8d705bca2ad4cac4ecd52e8328cacccc8
FFMG <github@myoddweb.com>
2016-03-03 22:26:33 +0200

    Updated the Python API version number to 2.0

--- monitor/api/pyapi.h
+++ monitor/api/pyapi.h
@@ -3,7 +3,9 @@
 #include "helperapi.h"
 
 // this is the version number for that particular API
-static const double ACTIONMONITOR_API_PY_VERSION = 0.1f;
+// 0.1 was the old API, not idea what version of Python it was using.
+// 2.0 uses version 3.5 of Python.
+static const double ACTIONMONITOR_API_PY_VERSION = 2.0;
 
 // support for Python
 #include "python\include\Python.h"
