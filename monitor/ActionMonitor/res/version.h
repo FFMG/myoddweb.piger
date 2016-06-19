92464a80983895355b6bcc9635b085a629e0214e
FFMG <github@myoddweb.com>
2016-06-19 14:03:46 +0200

    Updated version number.

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,9 +3,9 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 6
 #define VERSION_MAINTENANCE 10
-#define VERSION_BUILD 0
+#define VERSION_BUILD 1
 #define VERSION_BUILD_DATE  _T("19/06/2016")
-#define VERSION_BUILD_TIME  _T("11:59:11UTC")
+#define VERSION_BUILD_TIME  _T("14:03:59UTC")
 
 //
 // No need to edit below... (or maybe the year...)
