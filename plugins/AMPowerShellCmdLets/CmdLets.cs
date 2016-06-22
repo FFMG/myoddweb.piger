026ea87bc91449913f0c42a26b9013a98f7b446d
FFMG <github@myoddweb.com>
2016-06-22 06:26:35 +0200

    Started work on the Powershell commandlet

---
+++ plugins/AMPowerShellCmdLets/CmdLets.cs
@@ -0,0 +1,20 @@
+﻿using System.Management.Automation;
+
+namespace AMPowerShellCmdLets
+{
+  // http://www.powershellmagazine.com/2014/03/18/writing-a-powershell-module-in-c-part-1-the-basics/
+  // http://www.powershellmagazine.com/2014/04/08/basics-of-writing-a-powershell-module-with-c-part-2-debugging/
+  //
+  // Debug
+  //  C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+  //  -noexit -command "&{ import-module .\AMPowerShellCmdLets.dll -Verbose}"
+
+  [Cmdlet("Am", "Salutation")]
+  public class AmSalutation : Cmdlet
+  {
+    protected override void BeginProcessing()
+    {
+      WriteObject("BinaryModuleCmdlet2 exported by the ModuleCmdlets module.");
+    }
+  }
+}
