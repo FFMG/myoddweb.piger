a76ff392f5d43d2dd8c084412831fd35a5d5acc7
FFMG <github@myoddweb.com>
2016-06-27 22:19:55 +0200

    Added unit test(s) for ipcdata.

---
+++ plugins/AMPowerShellCmdLetsTest/ipcdatatest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AMPowerShellCmdLets.myodd;
+
+namespace AMPowerShellCmdLetsTest
+{
+  [TestClass]
+  public class ipcdatatest
+  {
+    [TestMethod]
+    public void TestHasGuid()
+    {
+      var emptyIpcData = new IpcData();
+      Assert.IsTrue(emptyIpcData.HasGuid());
+    }
+  }
+}
