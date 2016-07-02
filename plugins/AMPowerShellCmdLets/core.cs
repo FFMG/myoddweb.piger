b2c86bfd909717e30f205653bcc95586465a71f2
FFMG <github@myoddweb.com>
2016-07-02 06:30:15 +0200

    C# output files are moved to the x64/x86 projects.

--- plugins/AMPowerShellCmdLets/core.cs
+++ plugins/AMPowerShellCmdLets/core.cs
@@ -1,11 +1,12 @@
-﻿namespace Am
+﻿using System;
+
+namespace Am
 {
   // http://www.powershellmagazine.com/2014/03/18/writing-a-powershell-module-in-c-part-1-the-basics/
   // http://www.powershellmagazine.com/2014/04/08/basics-of-writing-a-powershell-module-with-c-part-2-debugging/
   //
   // Debug
   // C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
-  // -noexit -command "&{import-module .\AMPowerShellCmdLets.dll -Verbose}"
   // -noexit -command "&{Add-Type -Path .\AMPowerShellCmdLets.dll}"
   public class Core
   {
@@ -16,6 +17,7 @@
 
     public Core()
     {
+      //  create the connector.
       Connector = new MyOdd.IpcConnector(_Name);
     }
 
