222ae16aef66c1ba4839f40adbb6ce66896c7b5b
FFMG <github@myoddweb.com>
2016-07-13 07:40:35 +0200

    created x86 myoddtest and gtest. Fixed some project issues.

--- myoddtest/myoddtest64.cpp
+++ myoddtest/myoddtest64.cpp
@@ -2,11 +2,11 @@
 //
 
 #include "stdafx.h"
-#include <gtest\gtest.h>
+#include <gtest/gtest.h>
 
 int main(int argc, char** argv)
 {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+	printf("Running main() from main\n");
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
-
