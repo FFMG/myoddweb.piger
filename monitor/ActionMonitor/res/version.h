3559249700ebd79bf39f0427a22a4f88044e30fe
FFMG <github@myoddweb.com>
2016-08-23 13:07:07 +0200

    Fixed the location of the 'any.h' file.

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,10 +3,10 @@
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          8
 #define VERSION_MAINTENANCE    1
-#define VERSION_BUILD         29
+#define VERSION_BUILD         30
 
-#define VERSION_HOUR           9
-#define VERSION_MINUTE        38
+#define VERSION_HOUR          13
+#define VERSION_MINUTE         6
 
 #define VERSION_YEAR        2016
 #define VERSION_MONTH          8
