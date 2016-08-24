baadf5b23b6ecf6edaba3f52698b6156d5772dcb
FFMG <github@myoddweb.com>
2016-08-24 12:39:44 +0200

    Updated to sqllite 3.14.1

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,14 +3,14 @@
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          8
 #define VERSION_MAINTENANCE    1
-#define VERSION_BUILD          0
+#define VERSION_BUILD          1
 
-#define VERSION_HOUR          10
-#define VERSION_MINUTE        41
+#define VERSION_HOUR          12
+#define VERSION_MINUTE        39
 
 #define VERSION_YEAR        2016
 #define VERSION_MONTH          8
-#define VERSION_DAY           11
+#define VERSION_DAY           24
 
 //
 // No need to edit below...
