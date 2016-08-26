4817de1331ca13a304fff41b44b7a1f71f4ae2b9
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2016-08-26 08:35:47 +0200

    Added MIT copyright notices

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,10 +3,10 @@
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          8
 #define VERSION_MAINTENANCE    1
-#define VERSION_BUILD         49
+#define VERSION_BUILD         50
 
 #define VERSION_HOUR           8
-#define VERSION_MINUTE         3
+#define VERSION_MINUTE        34
 
 #define VERSION_YEAR        2016
 #define VERSION_MONTH          8
