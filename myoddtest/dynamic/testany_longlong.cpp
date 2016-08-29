4d2f814cb0d1c617be005a6cba8d3a7b24e8ec97
FFMG <github@myoddweb.com>
2016-08-29 19:34:15 +0200

    Added more tests

--- myoddtest/dynamic/testany_longlong.cpp
+++ myoddtest/dynamic/testany_longlong.cpp
@@ -73,3 +73,34 @@ TEST_MEM(AnyTestLongLong, CheckTheDataTypeUnsignedLongLongInt)
   ASSERT_EQ(myodd::dynamic::Type::Integer_unsigned_long_long_int, x.Type());
 }
 
+TEST_MEM(AnyTestLongLong, QuickPostAddToUnsignedLongLong)
+{
+  unsigned long long int value = std::numeric_limits<unsigned long long int>::max();
+  auto any = myodd::dynamic::Any(value -1 );
+  any++;
+  ASSERT_EQ(std::numeric_limits<unsigned long long int>::max(), any);
+}
+
+TEST_MEM(AnyTestLongLong, QuickPostAddToLongLong)
+{
+  long long int value = std::numeric_limits<long long int>::max();
+  auto any = myodd::dynamic::Any(value - 1);
+  any++;
+  ASSERT_EQ(std::numeric_limits<long long int>::max(), any);
+}
+
+TEST_MEM(AnyTestLongLong, QuickPreAddToUnsignedLongLong)
+{
+  unsigned long long int value = std::numeric_limits<unsigned long long int>::max();
+  auto any = myodd::dynamic::Any(value - 1);
+  ++any;
+  ASSERT_EQ(std::numeric_limits<unsigned long long int>::max(), any);
+}
+
+TEST_MEM(AnyTestLongLong, QuickPreAddToLongLong)
+{
+  unsigned long long int value = std::numeric_limits<long long int>::max();
+  auto any = myodd::dynamic::Any(value - 1);
+  ++any;
+  ASSERT_EQ(std::numeric_limits<long long int>::max(), any);
+}
