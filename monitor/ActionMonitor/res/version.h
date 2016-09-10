dd20cf128d9d25a83ba20520b2efd0649f699b6f
FFMG <github@myoddweb.com>
2016-09-10 07:18:52 +0200

    Added liscense information to memory cache.

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,14 +3,14 @@
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          8
 #define VERSION_MAINTENANCE    1
-#define VERSION_BUILD         87
+#define VERSION_BUILD         88
 
-#define VERSION_HOUR           8
-#define VERSION_MINUTE        59
+#define VERSION_HOUR           7
+#define VERSION_MINUTE        17
 
 #define VERSION_YEAR        2016
 #define VERSION_MONTH          9
-#define VERSION_DAY            8
+#define VERSION_DAY           10
 
 //
 // No need to edit below...
--- myodd/cache/memorycache.h
+++ myodd/cache/memorycache.h
@@ -1,3 +1,27 @@
+// ***********************************************************************
+// Copyright (c) 2016 Florent Guelfucci
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// @see https://opensource.org/licenses/MIT
+// ***********************************************************************
 #pragma once
 
 #include <string>
@@ -16,7 +40,7 @@ namespace myodd {
      *   - Be thread safe
      *   - Be fast
      *   - 'Look' similar to the .NET implementatation as much as posible.
-     *   - be c++11 compliant and require no other libraries.
+     *   - be c++14 compliant and require no other libraries.
      * @see https://msdn.microsoft.com/en-us/library/system.runtime.caching.memorycache(v=vs.110).aspx
      */ 
     class MemoryCache
