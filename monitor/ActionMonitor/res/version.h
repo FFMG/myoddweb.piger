58fef8b5c19cd85be3131eb58739d19199f52b61
FFMG <github@myoddweb.com>
2016-10-12 06:43:17 +0200

    Merged develop

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,14 +3,14 @@
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          8
 #define VERSION_MAINTENANCE    3
-#define VERSION_BUILD          1
+#define VERSION_BUILD          2
 
-#define VERSION_HOUR          10
-#define VERSION_MINUTE        48
+#define VERSION_HOUR           6
+#define VERSION_MINUTE        42
 
 #define VERSION_YEAR        2016
 #define VERSION_MONTH         10
-#define VERSION_DAY           11
+#define VERSION_DAY           12
 
 //
 // No need to edit below...
