aa17507d1eb0b705488c7aa01b7b9abb439e4bb2
FFMG <github@myoddweb.com>
2019-03-31 16:12:07 +0200

    Updated version number 0.9.2.1

--- monitor/ActionMonitor/res/version.h
+++ monitor/ActionMonitor/res/version.h
@@ -3,14 +3,14 @@
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          9
 #define VERSION_MAINTENANCE    2
-#define VERSION_BUILD          0
+#define VERSION_BUILD          1
 
-#define VERSION_HOUR          10
-#define VERSION_MINUTE        26
+#define VERSION_HOUR          11
+#define VERSION_MINUTE        48
 
 #define VERSION_YEAR        2019
 #define VERSION_MONTH          3
-#define VERSION_DAY           21
+#define VERSION_DAY           31
 
 //
 // No need to edit below...
