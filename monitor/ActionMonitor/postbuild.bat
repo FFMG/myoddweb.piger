1f7f8a1981177583f9b60faedc9b3cbff052dbe3
FFMG <github@myoddweb.com>
2022-07-01 08:08:33 +0200

    Updated batch file to throw errors on copy

--- monitor/ActionMonitor/postbuild.bat
+++ monitor/ActionMonitor/postbuild.bat
@@ -1,9 +1,24 @@
 @echo off 
 SET TargetDir=%~1
+SET /A TargetArch=%~2
 SET PythonDir=%TargetDir%\python\
 
-echo "%TargetDir%..\..\..\python64\*.*"
+if not exist "%PythonDir%" ( 
+	mkdir "%PythonDir%"
+)
 
-if not exist "%PythonDir%" mkdir "%PythonDir%"
-xcopy "%TargetDir%..\..\..\includes\python64\*.*" "%PythonDir%" /Y
-xcopy "%TargetDir%..\..\..\includes\python64d\*.*" "%PythonDir%" /Y
+if %TargetArch% == 86 (
+  echo Copying files for x86 Architecture
+	xcopy "%TargetDir%..\..\..\includes\python86\*.*" "%PythonDir%" /Y
+	xcopy "%TargetDir%..\..\..\includes\python86d\*.*" "%PythonDir%" /Y
+) else (
+  if %TargetArch% == 64 ( 
+    echo Copying files for x64 Architecture
+    xcopy "%TargetDir%..\..\..\includes\python64\*.*" "%PythonDir%" /Y
+    xcopy "%TargetDir%..\..\..\includes\python64d\*.*" "%PythonDir%" /Y
+  ) else (
+    echo Unknown or missing architecture.
+    set errorlevel=1
+    exit /b  
+  )
+)
